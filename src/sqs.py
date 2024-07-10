185feec2b48541804317cdb45671de867092f7de
Fido-Daniel <daniel@APP-D706H4C361-RD-DANIEL.local>
2024-07-10 15:58:05 +0000

    completed basics of sqs

--- src/sqs.py
+++ src/sqs.py
@@ -6,6 +6,7 @@ FIFO_QUEUE_NAME = "MY-TEST-FIFO-QUEUE.fifo"
 FIFO_QUEUE_URL = "https://sqs.eu-central-1.amazonaws.com/590183934493/MY-TEST-FIFO-QUEUE.fifo"
 DEAD_LETTER_QUEUE_NAME = 'DEAD-letter-queue-for-main'
 MAIN_QUEUE_NAME = 'MAIN-QUEUE'
+MAIN_QUEUE_URL = "https://sqs.eu-central-1.amazonaws.com/590183934493/MAIN-QUEUE"
 
 
 def sqs_client():
@@ -35,21 +36,48 @@ def create_queue_for_dead_letter():
 
 def create_main_queue_for_dead_letter():
     redrive_policy = {
-        "deadLetterTargetArn": "",
-        "maxAttempts": 3,
+        "deadLetterTargetArn": "arn:aws:sqs:eu-central-1:590183934493:DEAD-letter-queue-for-main",
+        "maxReceiveCount": 3,
     }
     return sqs_client().create_queue(
         QueueName=MAIN_QUEUE_NAME,
         Attributes={
-            "DelaySeconds": 0,
-            "MaximumMessageSize": 262144,
-            "VisibilityTimeout": 30,
-            "MessageRetentionPeriod": 345680,
-            "ReceiveMessageWaitTimeSeconds": 0,
+            "DelaySeconds": "0",
+            "MaximumMessageSize": "262144",
+            "VisibilityTimeout": "30",
+            "MessageRetentionPeriod": "345680",
+            "ReceiveMessageWaitTimeSeconds": "0",
             "RedrivePolicy": json.dumps(redrive_policy)
         }
     )
 
 
+def find_queue():
+    return sqs_client().list_queues(QueueNamePrefix=QUEUE_NAME)
+
+
+def list_queues():
+    return sqs_client().list_queues()
+
+
+def get_queue_attributes(queue_url):
+    return sqs_client().get_queue_attributes(
+        QueueUrl=queue_url,
+        AttributeNames=['All']
+    )
+
+
+def update_queue_attributes(queue_url, attributes):
+    return sqs_client().set_queue_attributes(
+        QueueUrl=queue_url,
+        Attributes=attributes
+    )
+
+
+def delete_queue_attributes(queue_url):
+    return sqs_client().delete_queue(QueueUrl=queue_url)
+
 if __name__ == '__main__':
-    print(create_fifo_sqs_queue())
+    print(
+        delete_queue_attributes(MAIN_QUEUE_URL)
+    )
